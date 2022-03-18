4b46c18e7a0aba9f2f2de1c5683015fedf984823
Jeeseob <jeeseob5761@gmail.com>
2022-03-18 16:07:55 +0900

    Posts 역순 정렬

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ from blog.models import Post
 def index(request):
 
     # DB에서 object를 가져오는 함수 (model기본 내장인듯)
-    posts = Post.objects.all();
+    posts = Post.objects.all().order_by('-pk');
+    # order_by('-pk') -> pk를 역순으로 정렬
 
     return render(
         request, # request에는 많은 정보가 들어있어, 이를 분석해서 처리할 수 있다.(os, 해상도, 등등 다양하게)
