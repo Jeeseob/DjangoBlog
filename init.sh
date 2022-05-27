b4343a8500721106a659ddc4c289008b578b3249
Jeeseob <jeeseob5761@gmail.com>
2022-05-27 17:00:11 +0900

    postgres db

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "------ create default admin user ------"
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@myapp.local', 'Passw0rd')" | python manage.py shell
